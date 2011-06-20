1b04f0b5bcefba2b7eab83b970db43c6dfe32ea8
Ivan Mushketik <proger.home@gmail.com>
2011-06-20 22:54:39 +0300

    Problem, NodeExpander classes implemented

--- src/aima/core/search/Framework.py
+++ src/aima/core/search/Framework.py
@@ -4,6 +4,108 @@ Created on Jun 19, 2011
 @author: Ivan Mushketik
 '''
 
+# Artificial Intelligence A Modern Approach (3rd Edition): page 66
+from abc import ABCMeta
+
+class Action(metaclass=ABCMeta):
+
+    def is_noop(self):
+        raise NotImplementedError()
+    
+
+#  Artificial Intelligence A Modern Approach (3rd Edition): page 67.
+class ActionFunction(metaclass=ABCMeta):
+
+    def actions(self, state):
+        """
+            Return set of actions that is possible in a current state
+            state - current state
+        """
+        raise NotImplementedError()
+
+# Artificial Intelligence A Modern Approach (3rd Edition): page 67.
+class ResultFunction(metaclass=ABCMeta):
+
+    def result(self, state, action):
+        """
+            Return a state that results from doing action in a current state
+            state - current state
+            action - action to do
+
+            return new state
+        """
+        raise NotImplementedError();
+
+# Artificial Intelligence A Modern Approach (3rd Edition): page 67
+class GoalTest(metaclass=ABCMeta):
+
+    def is_goal_state(self, state):
+        """
+        Check if current state is a goal state
+        Returns true if state is a goal state or false otherwise
+        """
+        raise NotImplementedError("GoalTest is an abstract class");
+
+
+# Artificial Intelligence A Modern Approach (3rd Edition): page 68
+class StepCostFunction(metaclass=ABCMeta):
+    def c(self, state, action, newState):
+        """
+            Calculate cost of taking action to perform state changing.
+
+            state - state from which action is to be performed
+            action - action to be performed
+            newState - state reached by performing an action
+
+            return - cost of performing an action
+        """
+        raise NotImplementedError("StepCostFunction is an abstract class");
+
+
+class DefaultStepCostFunction(StepCostFunction):
+
+    def __init__(self):
+        pass
+
+    """
+        Step cost function that returns 1 for every action
+    """
+    def c(self, state, action, newState):
+        return 1
+
+
+class Problem:
+    def __init__(self, initialState, actionsFunction, resultFuntion, goalTest,
+                        stepCostFunction=None):
+        self._initialState = initialState
+        self._actionsFunction = actionsFunction
+        self._resultFunction = resultFuntion
+        self._goalTest = goalTest
+
+        if stepCostFunction is not None:
+            self._stepCostFunction = stepCostFunction
+        else:
+            self._stepCostFunction = DefaultStepCostFunction()
+
+    def get_initial_state(self):
+        return self._initialState
+
+    def is_goal_state(self, state):
+        return self._goalTest.isGoalTest(state)
+
+    def get_goal_test(self):
+        return self._goalTest
+
+    def get_action_function(self):
+        return self._actionsFunction
+
+    def get_result_function(self):
+        return self._resultFunction
+
+    def get_step_cost_function(self):
+        return self._stepCostFunction
+
+# Artificial Intelligence A Modern Approach (3rd Edition): Figure 3.10, page 79
 class Node:
     def __init__(self, state, stepCost=0, parent=None, action=None):
         self._state = state        
@@ -15,67 +117,56 @@ class Node:
         else:
             self._pathCost = stepCost
             
-    def getState(self):
+    def get_state(self):
         return self._state
             
-    def getParent(self):
+    def get_parent(self):
         return self._parent
             
-    def getAction(self):
+    def get_action(self):
         return self._action
     
-    def getPathCost(self):
+    def get_path_cost(self):
         return self._pathCost
     
-    def isRootNode(self):
+    def is_root_node(self):
         return self._parent is None
     
-    def getPathFromRoot(self):
+    def get_path_from_root(self):
         node = self;
         path = []
         
         while node is not None:
             path.insert(0, node)
-            node = node.getParent()
+            node = node.get_parent()
         
         return path
     
     def __str__(self):
         return ""          
-        
-# Artificial Intelligence A Modern Approach (3rd Edition): page 67
-class GoalTest:
+
+
+
+
     
-    def isGoalState(self, state):
-        """
-        Check if current state is a goal state
-        Returns true if state is a goal state or false otherwise
-        """        
-        raise NotImplementedError("GoalTest is an abstract class");
-
-# Artificial Intelligence A Modern Approach (3rd Edition): page 68
-class StepCostFunction:
-    def c(self, state, action, newState):
-        
-        raise NotImplementedError("StepCostFunction is an abstract class");
-
 # Artificial Intelligence A Modern Approach (3rd Edition): page 78.
 class PathCostFunction:
     def g(self, node):
         """
         Return cost from the initial state to a current state.        
         """
-        return node.getPathCost()
-    
-    
-    
-    
+        return node.get_path_cost()
+
+
 class NodeExpander:
+    METRIC_NODES_EXPANDED = "nodesExpanded"
+
     def __init__(self):
-        self.metrics = {}
+        self._metrics = {}
+        self.clear_instrumentation()
             
-    def clearInstrumentation(self):
-        pass
+    def clear_instrumentation(self):
+        self._metrics[NodeExpander.METRIC_NODES_EXPANDED] = 0
         
     def getNodesExpanded(self):
         pass
@@ -83,5 +174,27 @@ class NodeExpander:
     def getMetrics(self):
         return self.metrics
         
-    def expandNode(self, node, problem):
-        pass
+    def expand_node(self, node, problem):
+        """
+
+        """
+        childNodes = []
+
+        currentState = node.get_state()
+        actionFunction = problem.get_action_function()
+        resultFunction = problem.get_result_function()
+        stepCostFunction = problem.get_step_cost_function()
+
+        for action in actionFunction.actions(currentState):
+            newState = resultFunction.result(currentState, action)
+            cost = stepCostFunction.c(currentState, action, newState)
+            newNode = Node(newState, cost, node, action)
+
+            childNodes.append(newNode)
+
+        self._metrics[NodeExpander.METRIC_NODES_EXPANDED] = self._metrics[NodeExpander.METRIC_NODES_EXPANDED] + 1
+        return childNodes
+
+
+class QueueSearch(NodeExpander):
+    pass
--- src/aima/core/search/tests/testFramework.py
+++ src/aima/core/search/tests/testFramework.py
@@ -11,37 +11,74 @@ from aima.core.search.Framework import *
 
 class TestNode(unittest.TestCase):
 
-    def testIsRootForNodeWithoutParent(self):
+    def test_is_root_for_node_without_parent(self):
         n1 = Node("state1")
-        self.assertTrue(n1.isRootNode(), "Node created without parent should be root")
+        self.assertTrue(n1.is_root_node(), "Node created without parent should be root")
         
-    def testIsRootForNodeWithParent(self):
+    def test_is_root_for_node_with_parent(self):
         n1 = Node("state1")
         n2 = Node("state2", 0, n1)
         
-        self.assertTrue(n1.isRootNode(), "Node created without parent should be root")
-        self.assertFalse(n2.isRootNode(), "Node created with parent node shouldn't be root")
-        self.assertEqual(n1, n2.getParent(), "Node.getParent() is broken")
+        self.assertTrue(n1.is_root_node(), "Node created without parent should be root")
+        self.assertFalse(n2.is_root_node(), "Node created with parent node shouldn't be root")
+        self.assertEqual(n1, n2.get_parent(), "Node.getParent() is broken")
         
-    def testGetTotalPathCost(self):
+    def test_get_total_path_cost(self):
         n1 = Node("state1", 5)
         n2 = Node("state2", 10, n1)
         n3 = Node("state3", 15, n2)
                 
-        self.assertEqual(30, n3.getPathCost(), "Path cost should be sum of cost of all nodes from root to a current node")
+        self.assertEqual(30, n3.get_path_cost(), "Path cost should be sum of cost of all nodes from root to a current node")
         
-    def testGetPathFromRoot(self):
+    def test_get_path_from_root(self):
         n1 = Node("state1", 5)
         n2 = Node("state2", 10, n1)
         n3 = Node("state3", 15, n2)
         
-        path = n3.getPathFromRoot()
+        path = n3.get_path_from_root()
         self.assertEquals(3, len(path), "Length of path should be equal to a number of nodes from current node to a root")
         self.assertEquals(n1, path[0])
         self.assertEquals(n2, path[1])
         self.assertEquals(n3, path[2])
-        
-    
+
+class TestAction(Action):
+    def is_noop(self):
+        return false
+
+class TestActionsFunction(ActionFunction):
+    def actions(self, state):
+        res = []
+        for i in range(0, 3):
+            res.append(TestAction())
+
+        return res
+
+class TestResultFuntion(ResultFunction):
+    def result(self, state, action):
+        return state + 1
+
+class TestGoalTest(GoalTest):
+    def is_goal_state(self, state):
+        return state == 3
+
+class TestProblem(Problem):
+    def __init__(self):
+        super().__init__(1, TestActionsFunction(), TestResultFuntion(), TestGoalTest())
+
+
+class TestNodeExpander(unittest.TestCase):
+    def test_node_expanding(self):
+        startNode = Node(1)
+        problem = TestProblem()
+        nodeExpander = NodeExpander()
+        actionsList = nodeExpander.expand_node(startNode, problem)
+
+        self.assertEqual(3, len(actionsList), "")
+        self.assertEqual(2, actionsList[0].get_state())
+        self.assertEqual(2, actionsList[1].get_state())
+        self.assertEqual(2, actionsList[2].get_state())
+
+
 
 
 if __name__ == "__main__":
